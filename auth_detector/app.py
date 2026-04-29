5edba6da5ed47d489b0644aab6cd4a788f0b0e03
differentcalliber08 <dhirajjadhav351@gmail.com>
2026-04-29 20:56:17 +0530

    Add initial authenticity check function

---
+++ auth_detector/app.py
@@ -0,0 +1,10 @@
+def check_authenticity(text):
+    return {
+        "ai_probability": "Coming soon",
+        "message": "Detection engine initializing"
+    }
+
+if __name__ == "__main__":
+    sample = "This is a test sentence."
+    result = check_authenticity(sample)
+    print(result)
